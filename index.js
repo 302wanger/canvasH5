1a098f6802591476e3417ff2ae3ef6102f185cfc
蛋炒饭302 <wangyuanfu315@gmail.com>
2018-07-03 18:45:51 +0800

    2

--- index.js
+++ index.js
@@ -33,13 +33,26 @@ var vueIntance = new Vue({
       Drawing.qrc("qrcanvas", this.imgPath, 150, 150);
     },
 
-    // updateCanvas: function() {
-    //   var canvas = document.getElementById("canvas");
+    updateCanvas: function() {
+      var canvas = document.getElementById("canvas");
+      ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = "black";
+      ctx.font = "20px Georgia";
+      ctx.fillText(this.exampleContent, 10, 50);
+    },
+
+    // drawCanvasUserImg: function() {
+    //   let canvas = document.getElementById("canvas");
     //   ctx = canvas.getContext("2d");
-    //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //   ctx.fillStyle = "black";
-    //   ctx.font = "20px Georgia";
-    //   ctx.fillText(this.exampleContent, 10, 50);
+
+    //   var codeimg = new Image();
+    //   codeimg.src =
+    //     "https://nos.netease.com/easyread/fle/a0df1d4009c7a2ec5fee/1524215500140/avatar.png";
+    //   codeimg.setAttribute("crossOrigin", "Anonymous");
+    //   img.onload = function() {
+    //     ctx.drawImage(codeimg, 10, 10, 72, 72);
+    //   };
     // },
 
     // 绘制背景图
@@ -60,6 +73,17 @@ var vueIntance = new Vue({
       var x_bot = height - 44;
       ctx.font = "19px Georgia";
 
+      //画上二维码
+      function convertCanvasToImage(canvas) {
+        var image = new Image();
+        image.src = canvas.toDataURL("image/png");
+        return image;
+      }
+      var mycans = document.getElementsByTagName("canvas")[1]; //二维码所在的canvas
+      var codeimg = convertCanvasToImage(mycans);
+      var xw = width - 72 - 29;
+      var xh = height - 6 - 72;
+
       // img.src =
       //   "https://nos.netease.com/easyread/fle/a0df1d4009c7a2ec5fee/1524215500140/avatar.png?" +
       //   +new Date();
@@ -69,22 +93,26 @@ var vueIntance = new Vue({
         ctx.drawImage(img, 0, 0, width, height);
         // 画文字
         ctx.fillText("Hello world", 10, 50);
+        // 画二维码
+        // this.drawCanvasUserImg();
+        ctx.drawImage(codeimg, xw, xh, 72, 72);
 
-        //绘制完成,转为图片
-        setTimeout(function() {
-          //在ios上无法在画完之后取到整个画布内容，加了个settimeout
-          var bigcan = document.getElementsByClassName("canvas")[0];
-          let images = new Image();
-          images.src = bigcan.toDataURL("image/png");
-          images.setAttribute("crossOrigin", "Anonymous");
-          document
-            .getElementsByClassName("canimg")
-            .attr("src", bigcan.toDataURL("image/png"));
-        }, 0);
+        // //绘制完成,转为图片
+        // setTimeout(function() {
+        //   //在ios上无法在画完之后取到整个画布内容，加了个settimeout
+        //   var bigcan = document.getElementsByTagName("canvas")[0];
+        //   let images = new Image();
+        //   images.src = bigcan.toDataURL("image/png");
+        //   images.setAttribute("crossOrigin", "Anonymous");
+        //   document
+        //     .getElementsByClassName("canimg")
+        //     .attr("src", bigcan.toDataURL("image/png"));
+        // }, 0);
       };
     }
   },
   mounted: function() {
     this.drawBgImage();
+    // this.drawCanvasUserImg();
   }
 });
